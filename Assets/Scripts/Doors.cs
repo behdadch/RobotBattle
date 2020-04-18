e6d90c720d947a2f5280b49983cd2f8ed7607cb1
behdad <behdadchalaki@gmail.com>
2020-04-18 19:45:29 -0400

    DoorTrigger+Rooms

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+    int count;
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+        count = 0; //No body has entered the door yet
+
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            DoorControl("Open");
+            count++;
+        }
+    }
+    void OnTriggerExit(Collider col){
+        count--;
+        if(doorOpen && count==0){
+            doorOpen = false;
+            DoorControl("Close");
+        }
+    }
+    void DoorControl(string status)
+    {
+        animator.SetTrigger(status);
+    }
+}
