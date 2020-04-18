7f9b28670c9ab59536e6d53bdb39a3016c82399b
Logan <giantpandagod@gmail.com>
2020-04-18 01:53:12 -0400

    Very Basic Multiplayer
    Implemented very basic multiplayer, where players can each launch the game and run around bumping into each other.
    
    1) Sometimes the client doesn't connect the first time
    2) The interactions between players is strangely glitchy

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Robot : Mirror.NetworkBehaviour {
+
+    [Header("Components")]
+    public NavMeshAgent agent;
+    public Animator animator;
+
+    [Header("Movement")]
+    public float moveSpeed = 5;
+    public float rotationSpeed = 100;
+
+//    [Header("Firing")]
+//    public KeyCode shootKey = KeyCode.Space;
+//    public GameObject projectilePrefab;
+//    public Transform projectileMount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // movement for local player
+        if (!isLocalPlayer)
+            return;
+
+        // rotate
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
+
+        // move
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        agent.velocity = forward * Mathf.Max(vertical, 0) * agent.speed;
+        animator.SetBool("Moving", agent.velocity != Vector3.zero);
+
+        // actions
+        
+    }
+}
