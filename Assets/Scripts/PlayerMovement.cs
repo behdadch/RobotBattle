bc518e0a4c9a59a94f40c7a971163dc8728fcb01
behdad <behdadchalaki@gmail.com>
2020-04-18 01:08:34 -0400

    initial

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    void Awake() {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        Vector3 movement = new Vector3();
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+
+        transform.position += movement * speed * Time.fixedDeltaTime;
+
+    }
+
+}
